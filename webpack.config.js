3625969c5ddbd83ec0d970cd3bff6e47e5e722db
kengo <38782494+skengo@users.noreply.github.com>
2021-08-19 14:41:47 +0900

    Optimize Images

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
     entry: './src/javascripts/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
@@ -12,18 +14,38 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css/,
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {'targets': '> 0.25%, not dead'}],
+                            ],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(css|sass|scss)/,
                 use: [
                     {
                         loader: MiniCssExtractPlugin.loader,
                     },
                     {
                         loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
                     },
                 ],
             },
             {
-                test: /\.(png|jpg)/,
+                test: /\.(png|jpg|jpeg|svg|gif)/,
                 type: 'asset/resource',
                 generator: {
                     filename: 'images/[name][ext]',
@@ -36,6 +58,15 @@ module.exports = {
                     //         name: 'images/[name].[ext]',
                     //     },
                     // },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65,
+                            },
+                        },
+                    },
                 ],
             },
             {
@@ -47,8 +78,8 @@ module.exports = {
                     {
                         loader: 'pug-html-loader',
                         options: {
-                            pretty: true
-                        }
+                            pretty: true,
+                        },
                     },
                 ],
             },
@@ -60,11 +91,15 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: './src/templates/index.pug',
-            filename: 'index.html'
+            filename: 'index.html',
         }),
         new HtmlWebpackPlugin({
             template: './src/templates/access.pug',
-            filename: 'access.html'
+            filename: 'access.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/members/taro.pug',
+            filename: 'members/taro.html',
         }),
         new CleanWebpackPlugin(),
     ],
